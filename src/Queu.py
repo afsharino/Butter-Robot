4d67add15f5b9e5c8a1412791817669096005098
Muhammed <mohammadafshari.fum.ac.ir@gmail.com>
2022-12-09 00:55:39 +0330

    Queue and Stack datastructures to store frontier in bfs and dfs

---
+++ src/Queu.py
@@ -0,0 +1,27 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self, node:Node):
+        self.queue.append(node)
+           
+    def dequeue(self):
+        try:
+            return self.queue.pop(0)
+        except:
+            return
+        
+    def front(self):
+        try:
+            return self.queue[0]
+        except:
+            return
+        
+    def enqueue(self, node:Node):
+        self.queue.append(node)
+    
+    def is_empty(self):
+        if len(self.queue) == 0:
+            return True
+        else:
+            return False
---
+++ src/Stack.py
@@ -0,0 +1,27 @@
+class Stack:
+    
+    def __init__(self):
+        self.stack = []
+    
+    def push(self, node:Node):
+        self.stack.append(node)   
+        
+    def pop(self):
+        try:
+            return self.stack.pop()
+        except:
+            return
+        
+    def top(self):
+        try:
+            return self.stack[-1]
+        except:
+            return
+    
+    def is_empty(self):
+        if len(self.stack) == 0:
+            return True
+        else:
+            return False
+        
+        
