86901c10cf1c0f024fc52e0754843e3a5d356a08
Muhammed <mohammadafshari.fum.ac.ir@gmail.com>
2022-12-09 04:42:28 +0330

    bfs search algorithm and rename module Queu to Queue.

---
+++ src/Algorithms/Uninformed/BFS.py
@@ -0,0 +1,29 @@
+import ButterRobot
+import Node
+import Queue
+from Utils import expand
+
+def breadth_first_search(problem:ButterRobot):
+    node = Node(state=problem.initial_state)
+    
+    if problem.is_goal(node.state):
+        return node
+    
+    frontier = Queue()
+    frontier.enqueue(node)
+    reached = [problem.initial_state]
+    
+    while not frontier.is_empty():
+        node = frontier.dequeue()
+        
+        for child in expand(problem, node):
+            state = child.state
+            
+            if problem.is_goal(state):
+                return child
+            
+            if state not in reached:
+                reached.append(state)
+                frontier.enqueue(child)
+                
+    return 
