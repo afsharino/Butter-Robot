cac0882b47a1dee76e57a6e2292765d94e71e05c
Muhammed <mohammadafshari.fum.ac.ir@gmail.com>
2022-12-09 04:35:54 +0330

    Utils module contains some utility functions including expand to expand childs of a node.

---
+++ src/Utils.py
@@ -0,0 +1,13 @@
+import Node
+import ButterRobot
+from copy import deepcopy
+
+def expand(problem:ButterRobot, node:Node):
+    state1 = node.state
+    
+    for action in problem.actions(state1):
+        state2 = problem.successor(deepcopy(state1), action)
+        cost = node.path_cost +  problem.action_cost(state2)
+        path = node.action_path + problem.get_direction(action)
+        depth = node.depth + 1
+        yield Node(state=state2, parent=node, action=action, path=path, depth=depth, cost=cost)
