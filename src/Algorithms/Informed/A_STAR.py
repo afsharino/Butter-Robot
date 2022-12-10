8fca713f89ceaf81895ec9ea5edf9d5a11e1485b
ehsan hosseini <ehosseini8001@gmail.com>
2022-12-10 05:36:43 +0330

    a_star added

---
+++ src/Algorithms/Informed/A_STAR.py
@@ -0,0 +1,27 @@
+from ButterRobot import ButterRobot
+from Node import Node
+from Queue import Queue
+
+
+def a_star(problem:ButterRobot):
+    node = Node(state=problem.initial_state)
+    
+    frontier = Queue()
+    frontier.a_star_enqueue(node)
+    reached ={}
+    reached[str(problem.initial_state)] = node
+    
+    while not frontier.is_empty():
+        node = frontier.dequeue()
+        
+        if problem.is_goal(node.state):
+            return node
+        
+        for child in expand(problem,node):
+            state = child.state
+            
+            if str(state) not in reached.keys() or\
+            (child.h_n + child.path_cost) < (reached[str(state)].h_n + reached[str(state)].path_cost):
+                reached[str(state)] = child
+                frontier.a_star_enqueue(child)
+    return
