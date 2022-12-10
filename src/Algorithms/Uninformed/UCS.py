e08d1ff0752aa6444ae43ee51015d9126e8bcf0a
ehsan hosseini <ehosseini8001@gmail.com>
2022-12-10 05:24:58 +0330

    UCS added

---
+++ src/Algorithms/Uninformed/UCS.py
@@ -0,0 +1,24 @@
+from queue import Queue
+from ButterRobot import ButterRobot
+from Node import Node
+from Utils import expand
+
+
+def uniform_cost_search(problem:ButterRobot):
+    node = Node(state=problem.initial_state)
+    frontier = Queue()
+    frontier.ucs_enqueue(node)
+    reached ={}
+    reached[str(problem.initial_state)] = node
+    
+    while not frontier.is_empty():
+        node = frontier.dequeue()
+        if problem.is_goal(node.state):
+            return node
+        for child in expand(problem,node):
+            state = child.state
+            if str(state) not in reached.keys() or\
+            child.path_cost < reached[str(state)].path_cost:
+                reached[str(state)] = child
+                frontier.ucs_enqueue(child)
+    return
